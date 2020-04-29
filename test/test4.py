d4b0e8b7524b7548da8c8124fb004c8af5269094
Your Name <you@example.com>
2020-04-29 16:39:37 +0600

    Test wasn't successful

--- test/test4.py
+++ test/test4.py
@@ -1,26 +1,160 @@
 import logging
+from telegram import ParseMode
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from datetime import datetime, time
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+CALLBACK_BUTTON1_LEFT = "callback_button1_left"
+CALLBACK_BUTTON2_RIGHT = "callback_button2_right"
+CALLBACK_BUTTON3_MORE = "callback_button3_more"
+CALLBACK_BUTTON4_BACK = "callback_button4_back"
+CALLBACK_BUTTON5_TIME = "callback_button5_time"
+CALLBACK_BUTTON6_PRICE = "callback_button6_price"
+CALLBACK_BUTTON7_PRICE = "callback_button7_price"
+CALLBACK_BUTTON8_PRICE = "callback_button8_price"
+
+TITLES = {
+    CALLBACK_BUTTON1_LEFT: "Новое сообщение ⚡️",
+    CALLBACK_BUTTON2_RIGHT: "Отредактировать ✏️",
+    CALLBACK_BUTTON3_MORE: "Ещё ➡️",
+    CALLBACK_BUTTON4_BACK: "Назад ⬅️",
+    CALLBACK_BUTTON5_TIME: "Время ⏰",
+    CALLBACK_BUTTON6_PRICE: "BTC 💰",
+    CALLBACK_BUTTON7_PRICE: "LTC 💰",
+    CALLBACK_BUTTON8_PRICE: "ETH 💰",
+}
+
+
+def get_base_inline_keyboard():
+    """ Получить клавиатуру для сообщения
+        Эта клавиатура будет видна под каждым сообщением, где её прикрепили
+    """
+    # Каждый список внутри `keyboard` -- это один горизонтальный ряд кнопок
+    keyboard = [
+        # Каждый элемент внутри списка -- это один вертикальный столбец.
+        # Сколько кнопок -- столько столбцов
+        [
+            InlineKeyboardButton(TITLES[CALLBACK_BUTTON1_LEFT], callback_data=CALLBACK_BUTTON1_LEFT),
+            InlineKeyboardButton(TITLES[CALLBACK_BUTTON2_RIGHT], callback_data=CALLBACK_BUTTON2_RIGHT),
+        ],
+        [
+            InlineKeyboardButton(TITLES[CALLBACK_BUTTON3_MORE], callback_data=CALLBACK_BUTTON3_MORE),
+        ],
+    ]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def get_keyboard2():
+    """ Получить вторую страницу клавиатуры для сообщений
+        Возможно получить только при нажатии кнопки на первой клавиатуре
+    """
+    keyboard = [
+        [
+            InlineKeyboardButton(TITLES[CALLBACK_BUTTON5_TIME], callback_data=CALLBACK_BUTTON5_TIME),
+        ],
+        [
+            InlineKeyboardButton(TITLES[CALLBACK_BUTTON6_PRICE], callback_data=CALLBACK_BUTTON6_PRICE),
+            InlineKeyboardButton(TITLES[CALLBACK_BUTTON7_PRICE], callback_data=CALLBACK_BUTTON7_PRICE),
+            InlineKeyboardButton(TITLES[CALLBACK_BUTTON8_PRICE], callback_data=CALLBACK_BUTTON8_PRICE),
+        ],
+        [
+            InlineKeyboardButton(TITLES[CALLBACK_BUTTON4_BACK], callback_data=CALLBACK_BUTTON4_BACK),
+        ],
+    ]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def keyboard_callback_handler(update, context):
+    """ Обработчик ВСЕХ кнопок со ВСЕХ клавиатур
+    """
+    query = update.callback_query
+    data = query.data
+    now = datetime.now()
+
+    # Обратите внимание: используется `effective_message`
+    chat_id = update.effective_message.chat_id
+    current_text = update.effective_message.text
+
+    if data == CALLBACK_BUTTON1_LEFT:
+        # "Удалим" клавиатуру у прошлого сообщения
+        # (на самом деле отредактируем его так, что текст останется тот же, а клавиатура пропадёт)
+        query.edit_message_text(
+            text=current_text,
+            parse_mode=ParseMode.MARKDOWN,
+        )
+        # Отправим новое сообщение при нажатии на кнопку
+        context.bot.send_message(
+            chat_id=chat_id,
+            text="Новое сообщение\n\ncallback_query.data={}".format(data),
+            reply_markup=get_base_inline_keyboard(),
+        )
+    elif data == CALLBACK_BUTTON2_RIGHT:
+        # Отредактируем текст сообщения, но оставим клавиатуру
+        query.edit_message_text(
+            text="Успешно отредактировано в {}".format(now),
+            reply_markup=get_base_inline_keyboard(),
+        )
+    elif data == CALLBACK_BUTTON3_MORE:
+        # Показать следующий экран клавиатуры
+        # (оставить тот же текст, но указать другой массив кнопок)
+        query.edit_message_text(
+            text=current_text,
+            reply_markup=get_keyboard2(),
+        )
+    elif data == CALLBACK_BUTTON4_BACK:
+        # Показать предыдущий экран клавиатуры
+        # (оставить тот же текст, но указать другой массив кнопок)
+        query.edit_message_text(
+            text=current_text,
+            reply_markup=get_base_inline_keyboard(),
+        )
+    elif data == CALLBACK_BUTTON5_TIME:
+        # Покажем новый текст и оставим ту же клавиатуру
+        text = "*Точное время*\n\n{}".format(now)
+        query.edit_message_text(
+            text=text,
+            parse_mode=ParseMode.MARKDOWN,
+            reply_markup=get_keyboard2(),
+        )
+    elif data in (CALLBACK_BUTTON6_PRICE, CALLBACK_BUTTON7_PRICE, CALLBACK_BUTTON8_PRICE):
+        pair = {
+            CALLBACK_BUTTON6_PRICE: "USD-BTC",
+            CALLBACK_BUTTON7_PRICE: "USD-LTC",
+            CALLBACK_BUTTON8_PRICE: "USD-ETH",
+        }[data]
+
+        try:
+            # current_price = client.get_last_price(pair=pair)
+            text = "*Курс валюты:*\n\n*{}* = 5000$".format(pair)
+        except:
+            text = "Произошла ошибка :(\n\nПопробуйте ещё раз"
+        query.edit_message_text(
+            text=text,
+            parse_mode=ParseMode.MARKDOWN,
+            reply_markup=get_keyboard2(),
+        )
+
 
 def start(update, context):
-    keyboard = [[InlineKeyboardButton("Yes", callback_data='1'),
-                 InlineKeyboardButton("No", callback_data='2')]]
+    context.bot.send_message(
+        chat_id=update.message.chat_id,
+        text="Привет!",
+        # reply_markup=get_base_inline_keyboard(),
+    )
+    lets_go = time(10, 38, 0)
+    context.job_queue.run_daily(get_base_inline_keyboard,
+                                time=lets_go,
+                                context=update.message.chat_id)
 
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    update.message.reply_text('Have you refused to read manga?:', reply_markup=reply_markup)
-
-
-def button(update, context):
-    query = update.callback_query
-    query.answer()
-    print(query.data)
-    query.edit_message_text(text="Selected option: {}".format(query.data))
+# def button(update, context):
+#     query = update.callback_query
+#     query.answer()
+#     print(query.data)
+#     query.edit_message_text(text="Selected option: {}".format(query.data))
 
 
 def help(update, context):
@@ -29,12 +163,13 @@ def help(update, context):
 
 def error(update, context):
     """Log Errors caused by Updates."""
+    print(context.error)
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
 updater = Updater("1102225020:AAFhHf9COV5oy4zzSPiD3DntW3wFZ1M1QgA", use_context=True)
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CallbackQueryHandler(button))
+updater.dispatcher.add_handler(CallbackQueryHandler(callback=keyboard_callback_handler))
 updater.dispatcher.add_handler(CommandHandler('help', help))
 updater.dispatcher.add_error_handler(error)
 
